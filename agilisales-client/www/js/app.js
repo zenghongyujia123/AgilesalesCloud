662e2761d857bea74a4027c4891d1580c8e9df18
yujia123 <1963968619@qq.com>
2015-12-29 22:35:03 +0800

    Fix error cause by default none exist page

--- agilisales-client/www/js/app.js
+++ agilisales-client/www/js/app.js
@@ -85,5 +85,5 @@ angular.module('agilisales', ['ionic', 'ngCordova'])
       });
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/home/playlists');
+    $urlRouterProvider.otherwise('/home/photos');
   });
