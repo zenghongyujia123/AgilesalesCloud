f7be03cd1e427042652d5d495cae95254214e2e2
yujia123 <1963968619@qq.com>
2016-01-07 16:31:40 +0800

    Change date picker init data

--- agilisales-client/www/controllers/daily.client.controller.js
+++ agilisales-client/www/controllers/daily.client.controller.js
@@ -4,15 +4,7 @@
 angular.module('agilisales')
   .controller('DailyCtrl', ['$scope', '$state', function ($scope, $state) {
     $scope.options = {
-      defaultDate: "2015-08-06",
-      minDate: "2015-01-01",
-      maxDate: "2015-12-31",
-      disabledDates: [
-        "2015-06-22",
-        "2015-07-27",
-        "2015-08-13",
-        "2015-08-15"
-      ],
+      defaultDate: new Date(),
       dayNamesLength: 1, // 1 for "M", 2 for "Mo", 3 for "Mon"; 9 will show full day names. Default is 1.
       mondayIsFirstDay: true,//set monday as first day of week. Default is false
       eventClick: function(date) { // called before dateClick and only if clicked day has events
