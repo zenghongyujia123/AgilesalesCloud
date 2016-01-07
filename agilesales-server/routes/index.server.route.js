4cb4f495ad3c9b4796f97c74677e8c5674466cc4
yujia123 <1963968619@qq.com>
2016-01-07 21:55:23 +0800

    Fix error cause by wrong name

--- agilesales-server/routes/index.server.route.js
+++ agilesales-server/routes/index.server.route.js
@@ -2,7 +2,7 @@
  * Created by zenghong on 16/1/7.
  */
 
-var contro = require('../routes/index');
+var indexCtrl = require('../routes/index');
 module.exports = function (app) {
-  app.route('/').get(userFilter.requireUser, dashboard.sortByOrderCount);
+  app.route('/').get(indexCtrl.index);
 };
