5181ee7dc5b0419f759cc10c8f3042a70babedc4
yujia123 <1963968619@qq.com>
2016-01-08 14:15:45 +0800

    Add photo panel for client

---
+++ agilesales-client/www/directives/photo_panel/photo.client.directive.js
@@ -0,0 +1,22 @@
+/**
+ * Created by zenghong on 15/12/27.
+ */
+angular.module('agilisales').directive('agPhotoPanel', [function () {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/map_panel/map.client.view.html',
+    replace: true,
+    scope: {},
+    controller: function ($scope, $element) {
+      $scope.show = function () {
+        $element.addClass('show');
+      };
+
+      $scope.hide = function () {
+        $element.removeClass('show');
+      };
+
+
+    }
+  };
+}]);
