397304ed2ef49080873a480e87e1e05f302b481b
yujia123 <1963968619@qq.com>
2016-01-10 23:25:33 +0800

    Add near by page style

--- agilesales-client/www/dist/js/agilisales.js
+++ agilesales-client/www/dist/js/agilisales.js
@@ -780,8 +780,14 @@ angular.module('agilisales')
  * Created by zenghong on 15/12/29.
  */
 angular.module('agilisales')
-  .controller('NearbyShopListCtrl', ['$scope', '$state', function ($scope, $state) {
+  .controller('NearbyShopListCtrl', ['$scope', '$state', '$rootScope', function ($scope, $state, $rootScope) {
+    $scope.showShopPanel = function () {
+      $rootScope.$broadcast('show.shopCreatePanel');
+    };
 
+    $scope.showMapPanel = function () {
+      $rootScope.$broadcast('show.mapPanel');
+    };
   }]);
 
 /**
