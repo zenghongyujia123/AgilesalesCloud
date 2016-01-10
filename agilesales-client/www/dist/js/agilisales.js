46e8bad0db68e909b5ab630f93842dfbae36fef2
yujia123 <1963968619@qq.com>
2016-01-10 21:48:47 +0800

    Update shop list page style

--- agilesales-client/www/dist/js/agilisales.js
+++ agilesales-client/www/dist/js/agilisales.js
@@ -341,74 +341,6 @@ angular.module('agilisales').directive('agFiltratePanel', [function () {
 /**
  * Created by zenghong on 15/12/27.
  */
-angular.module('agilisales').directive('agPhotoPanel', ['$cordovaCamera', function ($cordovaCamera) {
-  return {
-    restrict: 'AE',
-    templateUrl: 'directives/photo_panel/photo.client.view.html',
-    replace: true,
-    scope: {},
-    controller: function ($scope, $element) {
-      var options;
-      document.addEventListener("deviceready", function () {
-        options = {
-          quality: 50,
-          destinationType: Camera.DestinationType.DATA_URL,
-          sourceType: Camera.PictureSourceType.CAMERA,
-          //allowEdit: true,
-          encodingType: Camera.EncodingType.JPEG,
-          targetWidth: 100,
-          targetHeight: 100,
-          popoverOptions: CameraPopoverOptions,
-          saveToPhotoAlbum: false,
-          correctOrientation: true
-        };
-      }, false);
-
-
-      $scope.show = function () {
-        $element.addClass('show');
-      };
-
-      $scope.hide = function () {
-        $element.removeClass('show');
-      };
-
-      $scope.$on('show.photoPanel', function () {
-        $scope.show();
-      });
-
-      $scope.$on('show.photoBroswerPanel', function () {
-        $scope.showBrowserPanel()
-      });
-
-      $scope.showBrowserPanel = function () {
-        $element.find('.ag-photo-broswer-panel').addClass('show');
-      };
-
-      $scope.hideBrowserPanel = function () {
-        $element.find('.ag-photo-broswer-panel').removeClass('show');
-      };
-
-      $scope.photos = [];
-
-      $scope.getPicture = function () {
-        $cordovaCamera.getPicture(options).then(function (imageData) {
-
-          $scope.photos.push({
-            key: $scope.photos.length + 1,
-            value: "data:image/jpeg;base64," + imageData
-          });
-        }, function (err) {
-          console.log(err);
-        });
-      }
-    }
-  };
-}]);
-
-/**
- * Created by zenghong on 15/12/27.
- */
 angular.module('agilisales').directive('agMapPanel', ['$cordovaGeolocation', '$ionicPlatform', '$timeout', function ($cordovaGeolocation, $ionicPlatform, $timeout) {
   return {
     restrict: 'AE',
@@ -501,6 +433,74 @@ angular.module('agilisales').directive('agMapPanel', ['$cordovaGeolocation', '$i
           console.log(data);
         }
         geolocation.watchPosition();
+      }
+    }
+  };
+}]);
+
+/**
+ * Created by zenghong on 15/12/27.
+ */
+angular.module('agilisales').directive('agPhotoPanel', ['$cordovaCamera', function ($cordovaCamera) {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/photo_panel/photo.client.view.html',
+    replace: true,
+    scope: {},
+    controller: function ($scope, $element) {
+      var options;
+      document.addEventListener("deviceready", function () {
+        options = {
+          quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          //allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 100,
+          targetHeight: 100,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+          correctOrientation: true
+        };
+      }, false);
+
+
+      $scope.show = function () {
+        $element.addClass('show');
+      };
+
+      $scope.hide = function () {
+        $element.removeClass('show');
+      };
+
+      $scope.$on('show.photoPanel', function () {
+        $scope.show();
+      });
+
+      $scope.$on('show.photoBroswerPanel', function () {
+        $scope.showBrowserPanel()
+      });
+
+      $scope.showBrowserPanel = function () {
+        $element.find('.ag-photo-broswer-panel').addClass('show');
+      };
+
+      $scope.hideBrowserPanel = function () {
+        $element.find('.ag-photo-broswer-panel').removeClass('show');
+      };
+
+      $scope.photos = [];
+
+      $scope.getPicture = function () {
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+
+          $scope.photos.push({
+            key: $scope.photos.length + 1,
+            value: "data:image/jpeg;base64," + imageData
+          });
+        }, function (err) {
+          console.log(err);
+        });
       }
     }
   };
