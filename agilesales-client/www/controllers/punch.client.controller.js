7a63afa85e4c3d911fbfafde71ad41abb7175e4d
yujia123 <1963968619@qq.com>
2016-01-10 18:48:45 +0800

    Add punch page style

--- agilesales-client/www/controllers/punch.client.controller.js
+++ agilesales-client/www/controllers/punch.client.controller.js
@@ -2,12 +2,16 @@
  * Created by zenghong on 16/1/5.
  */
 angular.module('agilisales')
-  .controller('PunchCtrl', ['$scope', '$state', function ($scope, $state) {
+  .controller('PunchCtrl', ['$scope', '$rootScope', '$state', function ($scope, $rootScope, $state) {
     $scope.goDetail = function () {
       $state.go('menu.punch_detail');
     };
 
     $scope.goSubmit = function () {
       $state.go('menu.punch_submit');
-    }
+    };
+
+    $scope.showPhotoPanel = function () {
+      $rootScope.$broadcast('show.photoPanel');
+    };
   }]);
