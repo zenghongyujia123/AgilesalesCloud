2438d3b0104b5bed5255d15bd4352356efc970ba
yujia123 <1963968619@qq.com>
2016-01-13 14:40:49 +0800

    Add people select panel

--- agilesales-client/www/dist/js/agilisales.js
+++ agilesales-client/www/dist/js/agilisales.js
@@ -291,31 +291,6 @@ angular.module('agilisales').factory('NetworkTool', ['$cordovaNetwork', function
 /**
  * Created by zenghong on 15/12/27.
  */
-angular.module('agilisales').directive('agDailyCreatePanel', [function () {
-  return {
-    restrict: 'AE',
-    templateUrl: 'directives/daily_panel/daily_create.client.view.html',
-    replace: true,
-    scope: {},
-    controller: function ($scope, $element) {
-      $scope.show = function () {
-        $element.addClass('show');
-      };
-
-      $scope.hide = function () {
-        $element.removeClass('show');
-      };
-
-      $scope.$on('show.dailyCreatePanel', function () {
-        $scope.show();
-      });
-    }
-  };
-}]);
-
-/**
- * Created by zenghong on 15/12/27.
- */
 angular.module('agilisales').directive('agEventsSelectPanel', [function () {
   return {
     restrict: 'AE',
@@ -332,6 +307,31 @@ angular.module('agilisales').directive('agEventsSelectPanel', [function () {
       };
 
       $scope.$on('show.eventsSelectPanel', function () {
+        $scope.show();
+      });
+    }
+  };
+}]);
+
+/**
+ * Created by zenghong on 15/12/27.
+ */
+angular.module('agilisales').directive('agDailyCreatePanel', [function () {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/daily_panel/daily_create.client.view.html',
+    replace: true,
+    scope: {},
+    controller: function ($scope, $element) {
+      $scope.show = function () {
+        $element.addClass('show');
+      };
+
+      $scope.hide = function () {
+        $element.removeClass('show');
+      };
+
+      $scope.$on('show.dailyCreatePanel', function () {
         $scope.show();
       });
     }
