4ec0cdb52f890ce48fabbc7a927e62ee12adda54
yujia123 <1963968619@qq.com>
2016-01-14 16:15:03 +0800

    Update select question

--- agilesales-client/www/directives/questions/single_select/single_select.client.directive.js
+++ agilesales-client/www/directives/questions/single_select/single_select.client.directive.js
@@ -8,7 +8,8 @@ angular.module('agilisales').directive('agSingleSelectQuestion', [function () {
     template: ' <div class="ag-row-container ag-single-select-question"> \
                   <div class="ag-row-item">\
                     <div class="left">主陈列形式</div> \
-                    <select class="right">\
+                    <select class="right" placeho>\
+                      <option value="" disabled="true">请选择</option>\
                       <option ng-repeat="option in options">{{option}}</option> \
                     </select>\
                   </div> \
--- agilesales-client/www/dist/js/agilisales.js
+++ agilesales-client/www/dist/js/agilisales.js
@@ -665,31 +665,6 @@ angular.module('agilisales').directive('agStatisticsPanel', [function () {
 }]);
 
 /**
- * Created by zenghong on 16/1/14.
- */
-
-angular.module('agilisales').directive('agSingleSelectQuestion', [function () {
-  return {
-    restrict: 'AE',
-    template: ' <div class="ag-row-container ag-single-select-question"> \
-                  <div class="ag-row-item">\
-                    <div class="left">主陈列形式</div> \
-                    <select class="right">\
-                      <option ng-repeat="option in options">{{option}}</option> \
-                    </select>\
-                  </div> \
-                </div>',
-    replace: true,
-    scope: {},
-    link: function ($scope, $element, $attrs) {
-
-      $scope.options = $attrs.options || ['货架', '货架1'];
-      console.log($attrs);
-    }
-  };
-}]);
-
-/**
  * Created by zenghong on 15/12/27.
  */
 angular.module('agilisales').directive('agMultiSelectPanel', [function () {
@@ -710,6 +685,32 @@ angular.module('agilisales').directive('agMultiSelectPanel', [function () {
       $scope.$on('show.multiSelectPanel', function () {
         $scope.show();
       });
+    }
+  };
+}]);
+
+/**
+ * Created by zenghong on 16/1/14.
+ */
+
+angular.module('agilisales').directive('agSingleSelectQuestion', [function () {
+  return {
+    restrict: 'AE',
+    template: ' <div class="ag-row-container ag-single-select-question"> \
+                  <div class="ag-row-item">\
+                    <div class="left">主陈列形式</div> \
+                    <select class="right" placeho>\
+                      <option value="" disabled="true">请选择</option>\
+                      <option ng-repeat="option in options">{{option}}</option> \
+                    </select>\
+                  </div> \
+                </div>',
+    replace: true,
+    scope: {},
+    link: function ($scope, $element, $attrs) {
+
+      $scope.options = $attrs.options || ['货架', '货架1'];
+      console.log($attrs);
     }
   };
 }]);
