c44dca74649c2474f450b1c0dd90f468b4f6a0d5
yujia123 <1963968619@qq.com>
2016-01-14 16:10:11 +0800

    Add single question

--- agilesales-client/www/dist/js/agilisales.js
+++ agilesales-client/www/dist/js/agilisales.js
@@ -665,6 +665,31 @@ angular.module('agilisales').directive('agStatisticsPanel', [function () {
 }]);
 
 /**
+ * Created by zenghong on 16/1/14.
+ */
+
+angular.module('agilisales').directive('agSingleSelectQuestion', [function () {
+  return {
+    restrict: 'AE',
+    template: ' <div class="ag-row-container ag-single-select-question"> \
+                  <div class="ag-row-item">\
+                    <div class="left">主陈列形式</div> \
+                    <select class="right">\
+                      <option ng-repeat="option in options">{{option}}</option> \
+                    </select>\
+                  </div> \
+                </div>',
+    replace: true,
+    scope: {},
+    link: function ($scope, $element, $attrs) {
+
+      $scope.options = $attrs.options || ['货架', '货架1'];
+      console.log($attrs);
+    }
+  };
+}]);
+
+/**
  * Created by zenghong on 15/12/27.
  */
 angular.module('agilisales').directive('agMultiSelectPanel', [function () {
