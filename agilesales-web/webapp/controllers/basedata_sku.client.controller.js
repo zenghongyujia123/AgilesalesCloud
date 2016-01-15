8cd7f51f6162d17245ba6254f11bd09fb23f8ff4
yujia123 <1963968619@qq.com>
2016-01-15 23:25:53 +0800

    Add content for all basedata child view

--- agilesales-web/webapp/controllers/basedata_sku.client.controller.js
+++ agilesales-web/webapp/controllers/basedata_sku.client.controller.js
@@ -1,6 +1,14 @@
 /**
  * Created by zenghong on 16/1/15.
  */
-angular.module('agilesales-web').controller('BasedataSkuCtrl', function () {
+angular.module('agilesales-web').controller('BasedataSkuCtrl', ['$scope',function ($scope) {
+  $scope.peoples = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  $scope.values = [
+    '人员编号', '工号', '姓名',
+    '岗位', '职务', '电话号码',
+    '邮箱', '性别', '上级领导编号',
+    '上级领导姓名', '常驻城市', '辖区',
+    '帐号开通日期', '在职状态', '人员类型'
+  ];
 
-});
+}]);
