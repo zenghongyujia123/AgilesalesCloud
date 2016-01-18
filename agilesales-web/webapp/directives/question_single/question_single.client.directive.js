bac3639edd007b248d03b40ac68daba13a855908
yujia123 <1963968619@qq.com>
2016-01-18 17:56:36 +0800

    Add question single files

---
+++ agilesales-web/webapp/directives/question_single/question_single.client.directive.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zenghong on 16/1/18.
+ */
+angular.module('agilesales-web').controller('ag-question-single', function () {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/question_single/question_single.client.view.html',
+    replace: true,
+    scope: {},
+    link: function ($scope, $element, $attrs) {
+
+    }
+  }
+});
