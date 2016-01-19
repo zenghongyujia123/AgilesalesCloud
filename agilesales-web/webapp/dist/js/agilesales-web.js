4a2ebddb05645a05bbbe887ff162732dbb303494
yujia123 <1963968619@qq.com>
2016-01-19 17:25:19 +0800

    Add global directive control

--- agilesales-web/webapp/dist/js/agilesales-web.js
+++ agilesales-web/webapp/dist/js/agilesales-web.js
@@ -213,6 +213,19 @@ angular.module('agilesales-web').config(['$stateProvider', '$urlRouterProvider',
 //
 
 /**
+ * Created by zenghong on 16/1/19.
+ */
+angular.module('agilesales-web').directive('agGlobalEvent', ['$window', function ($window) {
+  return {
+    restrict: 'AE',
+    link: function ($scope, $element, $attrs) {
+      $(window).resize(function () {
+        $element.height($(this).height()-60);
+      }).resize();
+    }
+  }
+}]);
+/**
  * Created by zenghong on 16/1/18.
  */
 angular.module('agilesales-web').directive('agDialogConfirm', function () {
