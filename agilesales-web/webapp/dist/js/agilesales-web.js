345cb2a11da89531c2304c511c96a66da7285f86
yujia123 <1963968619@qq.com>
2016-01-19 16:39:51 +0800

    Add animate for pages

--- agilesales-web/webapp/dist/js/agilesales-web.js
+++ agilesales-web/webapp/dist/js/agilesales-web.js
@@ -229,6 +229,20 @@ angular.module('agilesales-web').directive('agDialogConfirm', function () {
 /**
  * Created by zenghong on 16/1/18.
  */
+angular.module('agilesales-web').directive('agDialogSelect', function () {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/dialog_select/dialog_select.client.view.html',
+    replace: true,
+    scope: {},
+    link: function ($scope, $element, $attrs) {
+
+    }
+  }
+});
+/**
+ * Created by zenghong on 16/1/18.
+ */
 angular.module('agilesales-web').directive('agDialogInput', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'AE',
@@ -266,20 +280,6 @@ angular.module('agilesales-web').directive('agDialogInput', ['$rootScope', funct
     }
   }
 }]);
-/**
- * Created by zenghong on 16/1/18.
- */
-angular.module('agilesales-web').directive('agDialogSelect', function () {
-  return {
-    restrict: 'AE',
-    templateUrl: 'directives/dialog_select/dialog_select.client.view.html',
-    replace: true,
-    scope: {},
-    link: function ($scope, $element, $attrs) {
-
-    }
-  }
-});
 /**
  * Created by zenghong on 16/1/18.
  */
