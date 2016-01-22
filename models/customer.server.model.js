ec6bfb7b6ca04bf1d818505a03ddb1932e5a09f5
yujia123 <1963968619@qq.com>
2016-01-22 15:05:37 +0800

    Add customer model

---
+++ models/customer.server.model.js
@@ -0,0 +1,79 @@
+/**
+ * Created by zenghong on 16/1/22.
+ */
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  timestamps = require('mongoose-timestamp');
+
+module.exports = function (appDb) {
+  var CustomerSchema = new Schema({
+    object: {
+      type: String,
+      default: 'customer'
+    },
+    name: {
+      type: String
+    },
+    short_name: {
+      type: String
+    },
+    number: {
+      type: String
+    },
+    //客户等级
+    customer_level: {
+      type: String
+    },
+    customer_type: {
+      type: String
+    },
+    channel_type: {
+      type: String
+    },
+    //客户地址
+    address: {
+      type: String
+    },
+    //客户送货地址
+    delivery_address: {
+      type: String
+    },
+    contact_phone: {
+      type: String
+    },
+    contact_telephone: {
+      type: String
+    },
+    contact_name: {
+      type: String
+    },
+    principal_number: {
+      type: String
+    },
+    principal_name: {
+      type: String
+    },
+    area_level1: {
+      type: String
+    },
+    area_level2: {
+      type: String
+    },
+    area_level3: {
+      type: String
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company'
+    }
+  });
+
+  CustomerSchema.plugin(timestamps, {
+    createdAt: 'created',
+    updatedAt: 'updated'
+  });
+
+  appDb.model('Customer', CustomerSchema);
+};
