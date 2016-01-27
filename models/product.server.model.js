03da45c8d77a03afe395795f71b13602c8c2f526
yujia123 <1963968619@qq.com>
2016-01-27 16:29:35 +0800

    Add product model for server

---
+++ models/product.server.model.js
@@ -0,0 +1,77 @@
+/**
+ * Created by zenghong on 16/1/27.
+ */
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  timestamps = require('mongoose-timestamp');
+
+module.exports = function (appDb) {
+  var ProductSchema = new Schema({
+    object: {
+      type: String,
+      default: 'product'
+    },
+    number:{
+      type:String
+    },
+    barcode:{
+      type:String
+    },
+    name:{
+      type:String
+    },
+    series:{
+      type:String
+    },
+    large_type:{
+      type:String
+    },
+    small_type:{
+      type:String
+    },
+    package_unit:{
+      type:String
+    },
+    //规格
+    specification:{
+      type:String
+    },
+    system_price:{
+      type:String
+    },
+    price:{
+      type:String
+    },
+    product_level:{
+      type:String
+    },
+    //是否新品
+    is_new:{
+      type:Boolean,
+      default:false
+    },
+    //是否重点产品
+    is_key:{
+      type:Boolean,
+      default:false
+    },
+    //是否档期促销品
+    is_promotion:{
+      type:Boolean,
+      default:false
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company'
+    }
+  });
+
+  ProductSchema.plugin(timestamps, {
+    createdAt: 'created',
+    updatedAt: 'updated'
+  });
+
+  appDb.model('Product', ProductSchema);
+};
