d714fa97728490ecdc4c2f362d0057e25c0cd351
yujia123 <1963968619@qq.com>
2016-01-28 17:06:24 +0800

    Add paper and question templates

--- models/card_template.server.model.js
+++ models/card_template.server.model.js
@@ -11,7 +11,44 @@ module.exports = function (appDb) {
   var CardTemplateSchema = new Schema({
     object: {
       type: String,
-      default: 'cardtemplate'
+      default: 'cardTemplate'
+    },
+    name: {
+      type: String
+    },
+    role: {
+      type: String
+    },
+    papers: [{
+      type: Schema.Types.Mixed
+    }],
+    customers: {
+      type: String
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company'
+    }
+  });
+
+  var PaperTemplateSchema = new Schema({
+    object: {
+      type: String,
+      default: 'paperTemplate'
+    },
+    questions: [{
+      type: Schema.Types.Mixed
+    }],
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company'
+    }
+  });
+
+  var QuestionTemplateSchema = new Schema({
+    object: {
+      type: String,
+      default: 'questionTemplate'
     },
     company: {
       type: Schema.Types.ObjectId,
@@ -24,5 +61,7 @@ module.exports = function (appDb) {
     updatedAt: 'updated'
   });
 
+  appDb.model('QuestionTemplate',QuestionTemplateSchema );
+  appDb.model('PaperTemplate', PaperTemplateSchema);
   appDb.model('CardTemplate', CardTemplateSchema);
 };
