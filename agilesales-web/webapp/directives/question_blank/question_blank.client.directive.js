11c269244c1826d8bd0c4e105b3a221e70f06356
yujia123 <1963968619@qq.com>
2016-02-02 16:05:11 +0800

    Update question directive

--- agilesales-web/webapp/directives/question_blank/question_blank.client.directive.js
+++ agilesales-web/webapp/directives/question_blank/question_blank.client.directive.js
@@ -1,14 +1,55 @@
 /**
  * Created by zenghong on 16/1/18.
  */
-angular.module('agilesales-web').directive('agQuestionBlank', function () {
+angular.module('agilesales-web').directive('agQuestionBlank', ['$rootScope', function ($rootScope) {
   return {
     restrict: 'AE',
     templateUrl: 'directives/question_blank/question_blank.client.view.html',
     replace: true,
     scope: {},
     link: function ($scope, $element, $attrs) {
+      $scope.question = {
+        type: 'blank',
+        type_text: '填空题',
+        input_type_text: '数字',
+        input_type: 'number',
+        title: '',
+        is_need_photo: false,
+        is_need_description: false
+      };
 
+      $scope.showInputType = function () {
+        $rootScope.$broadcast('show.dialogSelect', {
+          title: '输入方式',
+          contents: [{
+            key: '请选择输入方式',
+            value: '',
+            tip: '点击输入名称',
+            options: ['数字', '文本']
+          }],
+          color: 'blue',
+          callback: function (info) {
+            $scope.question.input_type_text = info.contents[0].value;
+            $scope.question.input_type = $scope.getInputType($scope.question.input_type_text);
+          }
+        });
+      };
+
+      $scope.togglePhoto = function () {
+        $scope.question.is_need_photo = !$scope.question.is_need_photo;
+      };
+      $scope.toggleDescription = function () {
+        $scope.question.is_need_description = !$scope.question.is_need_description;
+      };
+
+      $scope.getInputType = function (type) {
+        switch (type) {
+          case '数字':
+            return 'numbner';
+          case '文本':
+            return 'text';
+        }
+      }
     }
   }
-});
+}]);
--- agilesales-web/webapp/dist/js/agilesales-web.js
+++ agilesales-web/webapp/dist/js/agilesales-web.js
@@ -255,70 +255,6 @@ angular.module('agilesales-web').directive('agDialogInput', ['$rootScope', funct
 /**
  * Created by zenghong on 16/1/18.
  */
-angular.module('agilesales-web').directive('agDialogSelect', ['$rootScope', function ($rootScope) {
-  return {
-    restrict: 'AE',
-    templateUrl: 'directives/dialog_select/dialog_select.client.view.html',
-    replace: true,
-    scope: {},
-    link: function ($scope, $element, $attrs) {
-      $scope.options = [];
-      $scope.info = {
-        title: '',
-        contents: [{
-          key: '请输入拜访卡名称',
-          value: '点击输入名称'
-        }],
-        color: 'blue'
-      };
-
-      $scope.show = function () {
-        $element.addClass('show');
-        $element.find('.ag-dialog-panel').addClass('animated rotateIn');
-      };
-      $scope.hide = function () {
-        $element.removeClass('show');
-        $element.find('.ag-dialog-panel').removeClass('animated rotateIn')
-      };
-      $scope.submit = function () {
-        $element.removeClass('show');
-        $element.find('.ag-dialog-panel').removeClass('animated rotateIn')
-        $scope.info.callback($scope.info);
-      };
-      $scope.toggleOptions = function (index) {
-        if ($element.find('.ag-row-option-container').eq(index).hasClass('show')) {
-          $scope.hideOptions(index);
-        }
-        else {
-          $scope.showOptions(index);
-        }
-      };
-
-      $scope.selectOption = function (content, option) {
-        content.value = option;
-      };
-
-      $rootScope.$on('show.dialogSelect', function (event, data) {
-        setTheme(data);
-        $scope.show();
-      });
-      function setTheme(info) {
-        $element.find('.ag-dialog-panel').removeClass($scope.info.color).addClass(info.color);
-        $scope.info = info;
-      }
-
-      $scope.showOptions = function (index) {
-        $element.find('.ag-row-option-container').eq(index).addClass('show');
-      };
-      $scope.hideOptions = function (index) {
-        $element.find('.ag-row-option-container').eq(index).removeClass('show');
-      }
-    }
-  }
-}]);
-/**
- * Created by zenghong on 16/1/18.
- */
 angular.module('agilesales-web').directive('agDialogUpload', ['$rootScope', 'ExcelReaderService', function ($rootScope, ExcelReaderService) {
   return {
     restrict: 'AE',
@@ -383,24 +319,129 @@ angular.module('agilesales-web').directive('agDialogUpload', ['$rootScope', 'Exc
 /**
  * Created by zenghong on 16/1/18.
  */
+angular.module('agilesales-web').directive('agDialogSelect', ['$rootScope', function ($rootScope) {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/dialog_select/dialog_select.client.view.html',
+    replace: true,
+    scope: {},
+    link: function ($scope, $element, $attrs) {
+      $scope.options = [];
+      $scope.info = {
+        title: '',
+        contents: [{
+          key: '请输入拜访卡名称',
+          value: '点击输入名称'
+        }],
+        color: 'blue'
+      };
+
+      $scope.show = function () {
+        $element.addClass('show');
+        $element.find('.ag-dialog-panel').addClass('animated rotateIn');
+      };
+      $scope.hide = function () {
+        $element.removeClass('show');
+        $element.find('.ag-dialog-panel').removeClass('animated rotateIn')
+      };
+      $scope.submit = function () {
+        $element.removeClass('show');
+        $element.find('.ag-dialog-panel').removeClass('animated rotateIn')
+        $scope.info.callback($scope.info);
+      };
+      $scope.toggleOptions = function (index) {
+        if ($element.find('.ag-row-option-container').eq(index).hasClass('show')) {
+          $scope.hideOptions(index);
+        }
+        else {
+          $scope.showOptions(index);
+        }
+      };
+
+      $scope.selectOption = function (content, option) {
+        content.value = option;
+      };
+
+      $rootScope.$on('show.dialogSelect', function (event, data) {
+        setTheme(data);
+        $scope.show();
+      });
+      function setTheme(info) {
+        $element.find('.ag-dialog-panel').removeClass($scope.info.color).addClass(info.color);
+        $scope.info = info;
+      }
+
+      $scope.showOptions = function (index) {
+        $element.find('.ag-row-option-container').eq(index).addClass('show');
+      };
+      $scope.hideOptions = function (index) {
+        $element.find('.ag-row-option-container').eq(index).removeClass('show');
+      }
+    }
+  }
+}]);
+/**
+ * Created by zenghong on 16/1/18.
+ */
+angular.module('agilesales-web').directive('agQuestionBlank', ['$rootScope', function ($rootScope) {
+  return {
+    restrict: 'AE',
+    templateUrl: 'directives/question_blank/question_blank.client.view.html',
+    replace: true,
+    scope: {},
+    link: function ($scope, $element, $attrs) {
+      $scope.question = {
+        type: 'blank',
+        type_text: '填空题',
+        input_type_text: '数字',
+        input_type: 'number',
+        title: '',
+        is_need_photo: false,
+        is_need_description: false
+      };
+
+      $scope.showInputType = function () {
+        $rootScope.$broadcast('show.dialogSelect', {
+          title: '输入方式',
+          contents: [{
+            key: '请选择输入方式',
+            value: '',
+            tip: '点击输入名称',
+            options: ['数字', '文本']
+          }],
+          color: 'blue',
+          callback: function (info) {
+            $scope.question.input_type_text = info.contents[0].value;
+            $scope.question.input_type = $scope.getInputType($scope.question.input_type_text);
+          }
+        });
+      };
+
+      $scope.togglePhoto = function () {
+        $scope.question.is_need_photo = !$scope.question.is_need_photo;
+      };
+      $scope.toggleDescription = function () {
+        $scope.question.is_need_description = !$scope.question.is_need_description;
+      };
+
+      $scope.getInputType = function (type) {
+        switch (type) {
+          case '数字':
+            return 'numbner';
+          case '文本':
+            return 'text';
+        }
+      }
+    }
+  }
+}]);
+/**
+ * Created by zenghong on 16/1/18.
+ */
 angular.module('agilesales-web').directive('agQuestionSingle', function () {
   return {
     restrict: 'AE',
     templateUrl: 'directives/question_single/question_single.client.view.html',
-    replace: true,
-    scope: {},
-    link: function ($scope, $element, $attrs) {
-
-    }
-  }
-});
-/**
- * Created by zenghong on 16/1/18.
- */
-angular.module('agilesales-web').directive('agQuestionBlank', function () {
-  return {
-    restrict: 'AE',
-    templateUrl: 'directives/question_blank/question_blank.client.view.html',
     replace: true,
     scope: {},
     link: function ($scope, $element, $attrs) {
