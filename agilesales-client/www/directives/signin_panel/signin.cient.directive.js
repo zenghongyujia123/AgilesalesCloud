5fc21c02d74e5620c15ee98c42aa17cee18c84f2
yujia123 <1963968619@qq.com>
2016-02-04 16:27:57 +0800

    Update signin directive

--- agilesales-client/www/directives/signin_panel/signin.cient.directive.js
+++ agilesales-client/www/directives/signin_panel/signin.cient.directive.js
@@ -28,19 +28,21 @@ angular.module('agilisales').directive('agSigninPanel', ['$state', 'UserService'
 
         $scope.goSignin = function () {
           UserService.signin($scope.info.username, $scope.info.password).then(function (data) {
-            if (!data.err && data.access_token) {
-              AuthService.setToken(data.access_token);
-
-              UserService.getMe().then(function (data) {
-                if (!data.err) {
-                  AuthService.setUser(data);
-                  $scope.hide();
-                  $state.go('menu.home');
-                }
-              }, function (data) {
-
-              });
+            if (data.err) {
+              return alert(data.err.zh_message);
             }
+            AuthService.setToken(data.access_token);
+
+            UserService.getMe().then(function (data) {
+              if (data.err) {
+                return alert(data.err.zh_message);
+              }
+              AuthService.setUser(data);
+              $scope.hide();
+              $state.go('menu.home');
+            }, function (data) {
+
+            });
             console.log(data);
           }, function (data) {
             console.log(data);
